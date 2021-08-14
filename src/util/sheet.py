8840120ee262429551e0c09794e55e186f3abed4
WalleDong <747196076@qq.com>
2021-08-14 20:28:49 +0800

    export POSITION_CSV_PATH

--- src/util/sheet.py
+++ src/util/sheet.py
@@ -1,20 +1,22 @@
 import os
 import pandas as pd
 
-
-POSITION_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../config/position.csv")
+if os.getenv('POSITION_CSV_PATH', None):
+    POSITION_CSV_PATH = os.getenv('POSITION_CSV_PATH')
+else:
+    POSITION_CSV_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../config/position.csv")
 
 
 # 从 position.csv 中读取已购买基金的代码列表
 def read_buy_list():
-    df = pd.read_csv(POSITION_PATH, dtype={"code": str})
+    df = pd.read_csv(POSITION_CSV_PATH, dtype={"code": str})
     return df['code'].tolist()
 
 
 # 从 position.csv 中读取已购买基金的最新持仓
 def read_latest_position():
     latest_position = []
-    df = pd.read_csv(POSITION_PATH, dtype={"code": str})
+    df = pd.read_csv(POSITION_CSV_PATH, dtype={"code": str})
 
     for index, row in df.iterrows():
         latest_position.append((row['code'], row['position']))
