2efe5358d41f38ea65915dd8d33a70ab54d3105c
WalleDong <747196076@qq.com>
2021-09-02 23:51:32 +0800

    farmer

--- src/employees.py
+++ src/employees.py
@@ -23,140 +23,36 @@ class Employee:
 
 class Operator(Employee):
 
-    def add(self, code, amount, filed):
-        # 新买入基金
-        add_fund(code=code, 
-                 amount=amount,
-                 filed=filed,
-                 key=self.key
-                 )
-        update_total_for_field()
-
-    def delete(self, code):
-        # 删除基金
-        delete_fund(code=code, 
-                    key=self.key
-                    )
-        update_total_for_field()
-
-    def buy(self, code, amount):
-        # 加仓基金
-        buy_fund(code=code, 
-                 amount=amount, 
-                 key=self.key
-                 )
-        update_total_for_field()
-
-    def sell(self, code, amount):
-        # 卖出基金
-        sell_fund(code=code, 
-                  amount=amount, 
-                  key=self.key
-                  )
-        update_total_for_field()
-
     def record_op_auto(self):
-        # 从天天基金获取交易记录，自动将买入、卖出添加至数据库中
-        df = get_trade_record()
-        for index, row in df.iterrows():
-            if row['operate_type'] == OperateType.BUY:
-                buy_fund(code=row['code'], 
-                         amount=row['amount'],
-                         operate_time=row['operate_time'],
-                         key=self.key
-                         )
-            if row['operate_type'] == OperateType.SELL:
-                sell_fund(code=code, 
-                          amount=amount, 
-                          operate_time=row['operate_time'],
-                          key=self.key
-                          )
-        update_total_for_field()
-        return df
-
-    def update_position(self, code, amount):
-        # 更新基金的最新持仓
-        update_position(code=code, 
-                        amount=amount, 
-                        key=self.key
-                        )
-        update_total_for_field()
-
-    def update_position_list(self):
-        # 读取 position.csv 中基金的最新持仓，并更新持仓
-        latest_position = SheetTools.read_latest_position()
-        for code, position in latest_position:
-            update_position(code=code, 
-                            amount=position,
-                            key=self.key
-                            )
-        update_total_for_field()
-        return latest_position
+        print("record_op_auto")
 
     def update_position_auto(self):
-        # 从天天基金获取持仓数据，更新至数据库中
-        # todo: 等支付宝里面的都卖完了，这里简化
-        df = get_position()
-        from decimal import Decimal
-        code_list = df['code'].tolist()
-        latest_position = SheetTools.read_latest_position()
-        for code, position in latest_position:
-            if code in code_list:
-                p = df.loc[df['code'] == code]['position'].values[0] + Decimal(position).quantize(Decimal('0.00'))
-                df.at[df.loc[df['code'] == code].index, 'position'] = p
-            else:
-                df = df.append({'code': code,
-                                'position': Decimal(position).quantize(Decimal('0.00'))
-                                }, ignore_index=True)
-                
-        for index, row in df.iterrows():
-            update_position(code=row['code'], 
-                            amount=row['position'],
-                            key=self.key
-                            )
-        update_total_for_field()
+        print("update_position_auto")
 
-        return df
-
-
-    def get_dpt(self, code):
-        # 用于展示基金
-        return fund_dpt(code=code)
 
 
 class Statistician(Employee):
 
     def transport_netvalue(self):
-        # 把基金的历史净值上传至 db_netvalue 数据库中
-        return transport_netvalue()
+        print("transport_netvalue")
 
     def record_history(self):
-        # 统计并记录各个领域以及总的投入、持仓、收益历史
-        update_total_for_field()
-        record_history()
+        print("record_history")
 
     def export_tables(self):
-        # 导出基金最新数据总表、每个领域合计表、历史购买表、历史仓位表、历史收益表
-        SheetTools.export_tables()
+        print("export_tables")
 
     def draw_charts(self):
-        # 绘制图表
-        export_position_bar_chart()
-        export_profit_bar_chart()
-        export_position_profit_bar_chart()
-        export_profit_rate_bar_chart()
-        export_position_pie_chart()
+        print("draw_charts")
 
 
 class Analyst(Employee):
 
     def backtest(self):
-        # 回测，并将回测数据上传
-        transport_backtest_data()
+        print("backtest")
 
     def draw_backtest_plot(self):
-        # 绘制各领域基金回测的小提琴图
-        export_violin_plot()
+        print("draw_backtest_plot")
 
 
 
---
+++ src/farmer.py
@@ -0,0 +1,123 @@
+from chicken_farm.src.employees import Operator
+from chicken_farm.src.employees import Statistician
+from chicken_farm.src.employees import Analyst
+from chicken_farm.src.util.config import Config
+from chicken_farm.src.util.log import get_logger
+
+
+logger = get_logger(__file__)
+
+
+class Slave():
+    """
+    1、更新本周的操作记录，需要适配支付宝和天天基金
+    2、更新本周所有基金的持仓，需要适配支付宝和天天基金
+    3、统计并记录本周各个领域的投入、持仓、收益
+    4、导出个人数据统计表
+    5、导出个人数据统计图
+    6、把基金的历史净值上传至 db_netvalue 数据库中
+    7、更新回测分析数据
+    8、导出回测分析图表
+    """
+
+    def __init__(self, key):
+        self.operator = Operator(key)
+        self.statistician = Statistician(key)
+        self.analyst = Analyst(key)
+        self.reset()
+
+    def reset(self):
+        self._job = Job()
+
+    @property
+    def job(self):
+        job = self._job
+        self.reset()
+        return job
+
+    def record_op_auto(self):
+        self._job.add(self.operator.record_op_auto)
+
+    def update_position_auto(self):
+        self._job.add(self.operator.update_position_auto)
+
+    def record_history(self):
+        self._job.add(self.statistician.record_history)
+
+    def export_tables(self):
+        self._job.add(self.statistician.export_tables)
+
+    def draw_charts(self):
+        self._job.add(self.statistician.draw_charts)
+
+    def transport_netvalue(self):
+        self._job.add(self.statistician.transport_netvalue)
+
+    def backtest(self):
+        self._job.add(self.analyst.backtest)
+
+    def draw_backtest_plot(self):
+        self._job.add(self.analyst.draw_backtest_plot)
+
+
+class Job():
+
+    def __init__(self):
+        self.parts = []
+
+    def add(self, part):
+        self.parts.append(part)
+        
+    def run(self):
+        logger.info(f"Job parts: {[p.__name__ for p in self.parts]}")
+        for f in self.parts:
+            f()
+            logger.info(f"Run {f.__name__} success.")
+
+
+class Farmer:
+
+    def __init__(self):
+        self._slave = None
+
+    @property
+    def slave(self):
+        return self._slave
+
+    @slave.setter
+    def slave(self, slave):
+        self._slave = slave
+
+    def base_job(self):
+        self.slave.record_op_auto()
+
+    def full_job(self):
+        self.slave.record_op_auto()
+        self.slave.update_position_auto()
+        self.slave.record_history()
+        self.slave.export_tables() 
+        self.slave.draw_charts()
+        self.slave.transport_netvalue()
+        self.slave.backtest()
+        self.slave.draw_backtest_plot()
+
+
+if __name__ == "__main__":
+
+    farmer = Farmer()
+    slave = Slave(Config.operation_key)
+    farmer.slave = slave
+
+    print("Base job: ")
+    farmer.base_job()
+    slave.job.run()
+
+    print("Full job: ")
+    farmer.full_job()
+    slave.job.run()
+
+    # Remember, the Builder pattern can be used without a Director class.
+    print("Custom product: ")
+    slave.record_op_auto()
+    slave.update_position_auto()
+    slave.job.run()
