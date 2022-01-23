c3934b6d02dc24e3643de4c96d072608fb8ab06f
WalleDong <747196076@qq.com>
2022-01-23 17:02:11 +0800

    修复数字中有逗号的问题

--- src/farm.py
+++ src/farm.py
@@ -7,7 +7,7 @@ from ChickenFarm.src.module.statistics_mod import update_total_for_field, record
 from ChickenFarm.src.module.transport_mod import transport_netvalue, transport_netvalue_speed, transport_backtest_data
 from ChickenFarm.src.module.chrome_mod import get_trade_record, get_position
 from ChickenFarm.src.db.tbl_depository import get_fund_dic_from_dpt, get_filed_pd_from_dpt, get_all_pd_from_dpt
-from ChickenFarm.src.db.types import OperateType 
+from ChickenFarm.src.db.types import OperateType
 import ChickenFarm.src.plot.aip_plot as aip_plot
 import ChickenFarm.src.plot.statistics_plot as st_plot
 from ChickenFarm.src.util.tools import SheetTools
@@ -17,10 +17,10 @@ from ChickenFarm.src.util.log import get_logger
 
 logger = get_logger(__file__)
 pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows',None)
+pd.set_option('display.max_rows', None)
 pd.set_option('display.unicode.ambiguous_as_wide', True)
 pd.set_option('display.unicode.east_asian_width', True)
-pd.set_option('display.width', 180) # 设置打印宽度(**重要**)
+pd.set_option('display.width', 180)  # 设置打印宽度(**重要**)
 
 
 class SlaveBase:
@@ -42,31 +42,31 @@ class Operator(SlaveBase):
 
     def delete(self, code):
         # 删除基金
-        delete_fund(code=code, 
+        delete_fund(code=code,
                     key=self.key)
         update_total_for_field()
         print(colored(f"删除基金: {code}", "green"))
 
     def buy(self, code, amount):
         # 加仓基金
-        buy_fund(code=code, 
-                 amount=amount, 
+        buy_fund(code=code,
+                 amount=amount,
                  key=self.key)
         update_total_for_field()
         print(colored(f"{code} 买入 {amount} ¥。", "green"))
 
     def sell(self, code, amount):
         # 卖出基金
-        sell_fund(code=code, 
-                  amount=amount, 
+        sell_fund(code=code,
+                  amount=amount,
                   key=self.key)
         update_total_for_field()
         print(colored(f"{code} 卖出 {amount} ¥。", "green"))
 
     def update_position(self, code, amount):
         # 更新基金的最新持仓
-        update_position(code=code, 
-                        amount=amount, 
+        update_position(code=code,
+                        amount=amount,
                         key=self.key
                         )
         update_total_for_field()
@@ -91,14 +91,13 @@ class Operator(SlaveBase):
         print(funds_dp)
         return
 
-
     def record_op(self):
         # 从天天基金获取交易记录，自动将买入、卖出添加至数据库中
         df = get_trade_record()
         for index, row in df.iterrows():
             if row['operate_type'] == OperateType.BUY:
                 try:
-                    buy_fund(code=row['code'], 
+                    buy_fund(code=row['code'],
                              amount=row['amount'],
                              operate_time=row['operate_time'],
                              key=self.key)
@@ -107,17 +106,17 @@ class Operator(SlaveBase):
                     continue
                 except FundNotFoundError as error:
                     logger.warning(f"{error}")
-                    add_fund(code=row['code'], 
+                    add_fund(code=row['code'],
                              key=self.key)
-                    buy_fund(code=row['code'], 
+                    buy_fund(code=row['code'],
                              amount=row['amount'],
                              operate_time=row['operate_time'],
                              key=self.key
                              )
 
             if row['operate_type'] == OperateType.SELL:
-                sell_fund(code=row['code'], 
-                          amount=row['amount'], 
+                sell_fund(code=row['code'],
+                          amount=row['amount'],
                           operate_time=row['operate_time'],
                           key=self.key
                           )
@@ -125,7 +124,6 @@ class Operator(SlaveBase):
 
         print(colored(df, "green"))
         print(colored(f"自动记录买入、卖出操作完成。", "green"))
-
 
     def update_position_auto(self):
         # 从天天基金获取持仓数据，更新至数据库中
@@ -136,15 +134,16 @@ class Operator(SlaveBase):
         latest_position = SheetTools.read_latest_position()
         for code, position in latest_position:
             if code in code_list:
-                p = df.loc[df['code'] == code]['position'].values[0] + Decimal(position).quantize(Decimal('0.00'))
+                p = df.loc[df['code'] == code]['position'].values[0] + \
+                    Decimal(position).quantize(Decimal('0.00'))
                 df.at[df.loc[df['code'] == code].index, 'position'] = p
             else:
                 df = df.append({'code': code,
                                 'position': Decimal(position).quantize(Decimal('0.00'))
                                 }, ignore_index=True)
-                
+
         for index, row in df.iterrows():
-            update_position(code=row['code'], 
+            update_position(code=row['code'],
                             amount=row['position'],
                             key=self.key
                             )
@@ -152,7 +151,6 @@ class Operator(SlaveBase):
 
         print(colored(df, "green"))
         print(colored(f"自动更新持仓数据完成。", "green"))
-
 
 
 class Statistician(SlaveBase):
@@ -190,7 +188,7 @@ class Analyst(SlaveBase):
         print(colored(f"上传基金历史净值成功 {len(successes)} 条。", "green"))
         if len(fails) != 0:
             print(colored(f"上传基金历史净值失败 {len(fails)} 条。", "red"))
-    
+
     def transport_netvalue_speed(self):
         # 加速上传
         successes, fails = transport_netvalue_speed()
@@ -228,6 +226,7 @@ class Slave(Operator, Statistician, Analyst):
     """
     不可进行编排任务，直接使用
     """
+
     def add(self, code, filed):
         super().add(code, filed)
 
@@ -245,13 +244,14 @@ class Slave(Operator, Statistician, Analyst):
 
     def show(self, code, filed):
         super().show(code, filed)
-    
+
     def transport_netvalue_speed(self):
         super().transport_netvalue_speed()
 
     """
     可进行编排任务，需要 slave.job.run() 启动
     """
+
     def record_op(self):
         self._job.add(super().record_op)
 
@@ -284,7 +284,7 @@ class Job():
 
     def add(self, part):
         self.parts.append(part)
-        
+
     def run(self):
         logger.info(f"Job parts: {[p.__name__ for p in self.parts]}")
         for f in self.parts:
@@ -322,7 +322,7 @@ class Farmer:
         self.slave.record_op()
         self.slave.update_position_auto()
         self.slave.record_history()
-        self.slave.export_tables() 
+        self.slave.export_tables()
         self.slave.draw_charts()
         self.slave.transport_netvalue()
 
--- src/module/chrome_mod.py
+++ src/module/chrome_mod.py
@@ -2,24 +2,25 @@ import pandas as pd
 from datetime import datetime
 from decimal import Decimal
 
-from ChickenFarm.src.db.types import OperateType 
+from ChickenFarm.src.db.types import OperateType
 from ChickenFarm.src.util.chrome import ChromeDriver
 from ChickenFarm.src.util.log import get_logger
 
 
 logger = get_logger(__file__)
 pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows',None)
+pd.set_option('display.max_rows', None)
 pd.set_option('display.unicode.ambiguous_as_wide', True)
 pd.set_option('display.unicode.east_asian_width', True)
-pd.set_option('display.width', 180) # 设置打印宽度(**重要**)
+pd.set_option('display.width', 180)  # 设置打印宽度(**重要**)
 
 
 def get_trade_record():
 
     records = ChromeDriver().query_trade_records()
 
-    df = pd.DataFrame(columns=['operate_time', 'name', 'code', 'operate_type', 'amount'])
+    df = pd.DataFrame(columns=['operate_time', 'name',
+                      'code', 'operate_type', 'amount'])
     for r in records:
         r = r.replace(' ', '\n').split('\n')
         if "已撤单" in r[10] or "无效" in r[10]:
@@ -31,13 +32,13 @@ def get_trade_record():
         else:
             continue
 
-        df = df.append({'operate_time': datetime.strptime(r[0]+' '+r[1], '%Y-%m-%d %H:%M:%S'), 
+        df = df.append({'operate_time': datetime.strptime(r[0]+' '+r[1], '%Y-%m-%d %H:%M:%S'),
                         'name': r[2],
                         'code': r[3],
                         'operate_type': operate_type,
                         'amount': Decimal(r[5][:-1]).quantize(Decimal('0.00'))
                         }, ignore_index=True)
-    
+
     df = df.iloc[::-1]
     return df
 
@@ -48,13 +49,15 @@ def get_position():
 
     df = pd.DataFrame(columns=['name', 'code', 'position'])
     for p in positions:
-        if len(p) == 0: continue
-        if '010718' in p: continue  # 010718这个基金不纳入持仓统计
+        if len(p) == 0:
+            continue
+        if '010718' in p:
+            continue  # 010718这个基金不纳入持仓统计
         p = p.replace(' ', '\n').split('\n')
-
-        df = df.append({'name': p[0][:-8], 
+        print(p)
+        df = df.append({'name': p[0][:-8],
                         'code': p[0][-7:-1],
-                        'position': Decimal(p[2]).quantize(Decimal('0.00'))
+                        'position': Decimal(p[2].replace(",", "")).quantize(Decimal('0.00'))
                         }, ignore_index=True)
 
     return df
